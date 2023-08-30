81259592d6bd524250cfeb28a63957e2666a20f1
abigailew457 <abigailew457@gmail.com>
2023-08-30 19:37:37 +0900

    Create rows and columns divs

--- javascript.js
+++ javascript.js
@@ -1,2 +1,6 @@
 
 const container = document.querySelector('#container');
+const rows = document.createElement('div');
+rows.className = ('rows');
+const columns = document.createElement('div');
+columns.className = ('columns');
