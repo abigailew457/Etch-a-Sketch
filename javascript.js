66608482c41f1f3ef8572a282ab153fc73d0697b
abigailew457 <abigailew457@gmail.com>
2023-08-30 20:09:31 +0900

    Append rows and columns into container

--- javascript.js
+++ javascript.js
@@ -1,6 +1,9 @@
-
 const container = document.querySelector('#container');
+console.log(container);
+
 const rows = document.createElement('div');
 rows.className = ('rows');
 const columns = document.createElement('div');
 columns.className = ('columns');
+
+document.getElementById("container").appendChild(rows, columns);
