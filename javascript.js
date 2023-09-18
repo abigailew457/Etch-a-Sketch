fab39bad8a0473f7ffb6b06bb1791f222e946a74
abigailew457 <abigailew457@gmail.com>
2023-09-18 21:15:15 +0900

    Add size prompt

--- javascript.js
+++ javascript.js
@@ -11,12 +11,12 @@ textCont.appendChild(btn);
 function makeRows(rows, cells) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cells', cells);
-
+    
     for (i = 0; i < (rows * cells); i++) {
         let cell = document.createElement('div');
         cell.classList.add('cell');
         container.appendChild(cell);
-          
+        
         cell.addEventListener("mouseover", colorChange);
         function colorChange() {
             cell.style.backgroundColor = randomColor;
@@ -26,12 +26,11 @@ function makeRows(rows, cells) {
         function reset() {
             window.location.reload();
         } 
-}
+}  
 };
 
-makeRows(16, 16);
+let gridPrompt = prompt('How big you want her?');
 
+makeRows(gridPrompt, gridPrompt);
 
 var randomColor = Math.floor(Math.random()*16777215).toString(16);
-
-
